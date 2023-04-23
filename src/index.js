703c22818363f0c1f9e5c8060bba3256ba9ce98b
Chris Buckley <chris@cmbuckley.co.uk>
2023-04-23 19:33:35 +0100

    Initial commit of Cloudflare Workers version

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import config from '../config/links';
+import plugins from './plugins';
+
+const allPatterns = new RegExp(config.patterns.map(p => p.pattern).join('|'));
+config.patterns.forEach((p, i) => config.patterns[i].regexp = new RegExp(p.pattern));
+
+function getResponse(options, requestUrl) {
+    if (options.plugin) {
+        options = plugins.run(dest);
+    }
+
+    if (options.url) {
+        return Response.redirect(options.url);
+    }
+
+    if (options.body) {
+        const headers = [];
+
+        if (options.type) {
+            headers['Content-Type'] = options.type;
+        }
+
+        return new Response(options.body, {headers});
+    }
+
+    if (options[0] == '/') {
+        options = requestUrl.protocol + options;
+    }
+
+    return Response.redirect(options);
+}
+
+export default {
+    async fetch(request, env, ctx) {
+        const url = new URL(request.url);
+        const path = url.pathname.replace(/^\//, '');
+
+        if (config.static[path]) {
+            let dest = config.static[path];
+            return getResponse(dest, url);
+        }
+        else if (allPatterns.test(path)) {
+            const pattern = config.patterns.find(p => p.regexp.test(path));
+            let target = pattern.target;
+
+            if (typeof target == 'string') {
+                target = path.replace(pattern.regexp, target);
+            }
+
+            return getResponse(target, url);
+        }
+
+        return new Response(404);
+    }
+};
